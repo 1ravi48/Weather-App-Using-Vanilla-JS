4792c20052aaa86addb439759138e13c52bae955
Erin Shien <erinshiensmith@gmail.com>
2024-02-23 11:34:33 +0000

    added images and set them to change depending on weather type

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ let windSpeedHTML = document.getElementById("wind-speed");
 let sunriseHTML = document.getElementById("sunrise");
 let sunsetHTML = document.getElementById("sunset"); 
 
+let weatherImage = document.getElementById("weather-image");
+
 
 const weatherUrl = "https://api.open-meteo.com/v1/forecast?latitude=54.072&longitude=-2.2885&current=temperature_2m,weather_code,wind_speed_10m&daily=sunrise,sunset&timezone=GMT&forecast_days=1"
 
@@ -47,24 +49,31 @@ let weatherTypeString = ""
 
 if (weatherTypeJava === 0) {
     weatherTypeString="Clear sky"
+    weatherImage.setAttribute("src", "icons/sun_1163662.png")
 }
 else if (0<weatherTypeJava<4){
     weatherTypeString="Overcast"
+    weatherImage.setAttribute("src", "icons/cloudy_1163661.png")
 }
 else if (weatherTypeJava===45 || weatherTypeJava===48){
     weatherTypeString="Fog"
+    weatherImage.setAttribute("src", "icons/foog_1163640.png")
 }
 else if (50<weatherTypeJava<58 || 79<weatherTypeJava<83){
     weatherTypeString="Rain"
+    weatherImage.setAttribute("src", "icons/rainy_1163627.png")
+
 }
 else if (70<weatherTypeJava<78 || 84<weatherTypeJava<87){
     weatherTypeString="Snow"
+    weatherImage.setAttribute("src", "icons/snowy_1163629.png")
 }
 else if (94<weatherTypeJava<100){
     weatherTypeString="Thunderstorms"
+    weatherImage.setAttribute("src", "icons/storm_1163636.png")
 }
 
-temperatureHTML.textContent = "Temperature: " + temperatureJava + "°C";
+temperatureHTML.textContent = temperatureJava + "°C";
 weatherTypeHTML.textContent = weatherTypeString; 
 windSpeedHTML.textContent = "Wind speed: " + windSpeedJava + "km/h"; 
 sunriseHTML.textContent = "Sunrise: " + sunriseTime + "am"; 
